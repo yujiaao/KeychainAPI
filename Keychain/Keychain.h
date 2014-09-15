f5292b2652283d04364d28b29507bd52f6faafa0
Justin Williams <justin@secondgearsoftware.com>
2014-09-15 10:21:05 -0600

    Initial Commit

---
+++ Keychain/Keychain.h
@@ -0,0 +1,19 @@
+//
+//  Keychain.h
+//  Keychain
+//
+//  Created by Justin Williams on 9/15/14.
+//  Copyright (c) 2014 Second Gear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Keychain.
+FOUNDATION_EXPORT double KeychainVersionNumber;
+
+//! Project version string for Keychain.
+FOUNDATION_EXPORT const unsigned char KeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Keychain/PublicHeader.h>
+
+
